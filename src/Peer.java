c4441cb1c9d5cc7b7d8ddc9bb84328b1c0235737
Jacques-Antoine (Jack) Portal <jacquesantoine.portal@gmail.com>
2018-11-20 20:38:54 +0100

    Added a Crypto Algorithm class and a few basic parameters to the peer class

--- src/Peer.java
+++ src/Peer.java
@@ -2,4 +2,15 @@
  *
  */
 public class Peer {
+    public final int publicKey;
+    public String[] fileNames;
+    public String[] AvailableFiles;
+
+    private final int privateKey;
+    private String[] nodes;
+
+    public Peer(int publicKey, int privateKey) {
+        this.publicKey = publicKey;
+        this.privateKey = privateKey;
+    }
 }
