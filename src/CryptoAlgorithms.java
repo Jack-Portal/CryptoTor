135169fd4a168145cb41b9c5e23012aba6d242fb
Jacques-Antoine (Jack) Portal <jacquesantoine.portal@gmail.com>
2018-11-25 01:36:22 +0100

    All the possible messages that can be sent from one entity to another. Still need a few modification of the README file to reflect this system architecture.

--- src/CryptoAlgorithms.java
+++ src/CryptoAlgorithms.java
@@ -1,5 +1,21 @@
 /**
  *
  */
-public class CryptoAlgorithms {
+public abstract class CryptoAlgorithms {
+
+    public String symEncryption(String message, int key){
+        return message;
+    }
+
+    public String symDecryption(String message, int key){
+        return message;
+    }
+
+    public String asymEncryption(String message, int pbKey){
+        return message;
+    }
+
+    public String asymDecryption(String message, int pvKey){
+        return message;
+    }
 }
--- src/Node.java
+++ src/Node.java
@@ -1,10 +1,60 @@
 /**
  *
  */
-public class Node {
 
-    private void TrasnferPacket(){
-        // decrypt packet received
-        // transfer to next node
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class Node extends Thread{
+
+    public int pbKey;
+    public int port;
+    public Tracker tracker;
+    private int pvKey;
+    private Map<Node, Integer> symKeys;
+    private Map<String,Node> cookies;
+
+
+    public void Node(int port, Tracker tracker){
+        // Choose pbk and pvk
+        // initialise empty sym key and cookie
+    }
+
+
+    private void newConnection(String newConnectionMessage){
+        // decrypt with private key
+        // transfer
+        // if not last node:
+            // wait for answer
+        // generate symmetric key
+        // make message
+        // encrypt message with previous node's public key
+        // send response
+    }
+
+
+    private void transfer(String encryptedMessage){
+        // get symmetric key
+        // decrypt message
+        // if not last node:
+            // find next node
+        // transfer the message
+    }
+
+    private void performAction(String decryptedMessage){
+        // get what to do from the message (probably massive switch)
+        // functional programming ? do that action?
+    }
+
+    @Override
+    public void run(){
+        // create a server / socket
+        // connect to the Tracker and announce itself
+        while(true){
+            // wait for request
+            // completableFuture.supplyAsync(()->....
+                // decrypt
+                // performAction
+        }
     }
 }
